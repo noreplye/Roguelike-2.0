89b5c3f5d9a6b85c39a8925e948688f63884f15d
t00sh0rt <maksimfpm28@gmail.com>
2023-03-23 08:32:06 +1000

    чето там поправил и чето сделал с функцией battle

--- Map.cs
+++ Map.cs
@@ -71,7 +71,7 @@ public class map
         }
     }
     */
-    public static void init_Event(map current_map)
+    public static void init_Event(map current_map,Person character)
     {
         if (current_map._mapObjects[current_map.player_yPos, current_map.player_xPos] == 82)
         {
@@ -106,6 +106,12 @@ public class map
         {
             Room BossRoom = new Boss_room();
             init_newRoom(current_map, BossRoom);
+            current_map._mapObjects[current_map.player_yPos, current_map.player_xPos] = 1;
+            current_map._mapGraphics[current_map.player_yPos, current_map.player_xPos] = '.';
+        }
+        if (current_map._mapObjects[current_map.player_yPos, current_map.player_xPos] == 3)
+        {
+            Person.Battle(character);
             current_map._mapObjects[current_map.player_yPos, current_map.player_xPos] = 1;
             current_map._mapGraphics[current_map.player_yPos, current_map.player_xPos] = '.';
         }
@@ -199,26 +205,49 @@ public class map
 
     internal class Program
     {
+        private static Factory GetMyPerson(int ClassSelection) =>
+        ClassSelection switch
+        {
+            1 => new GetKnight(),
+            2 => new GerArcher(),
+            3 => new GetThief(),
+            _ => null
+        };
         static void Main(string[] args)
         {
             int game = 1;
-
             ConsoleKeyInfo pushed_button;
             map karta = new map();
             map.init_StartRoom(karta);
             Console.WriteLine("Сначала сделайте полный экран (F11).\nЗатем нажмите любую клавишу");
             Console.ReadKey();
             Console.Clear();
+            Console.Write("Введите имя вашего персонажа: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Выберите класс вашего персонажа: \n 1. Рыцарь \n 2. Лучник \n 3. Вор\n");
+            int ClassSelection = Convert.ToInt32(Console.ReadLine());
+
+            Factory factory = GetMyPerson(ClassSelection);
+            Person character = factory.GetMyPerson();
+            character.TakeName(name);
+            character.ShowStats();
+            Console.Clear();
             while (game > 0)
             {
-                    map.init_Event(karta);
-                    map.display(20, 20, karta);
-                    pushed_button = Console.ReadKey();
-                    map.Move(karta, pushed_button);
-            
+                map.init_Event(karta, character);
+                map.display(50, 20, karta);
+                character.ShowStats();
+                pushed_button = Console.ReadKey();
+                map.Move(karta, pushed_button);
+                if (character.Health <= 0)
+                {
+                    Console.Clear();
+                    Console.WriteLine("GAME OVER");
+                    game = 0;
+                }
             }
         }
     }
-}
 
 */
--- Person.cs
+++ Person.cs
@@ -18,7 +18,7 @@ class GerArcher : Factory
 {
     public override Person GetMyPerson()
     {
-        Archer archer = new(111, 111, 111, 111, 111, "");
+        Archer archer = new(20, 111, 111, 111, 111, "");
         return archer;
     }
 
@@ -35,7 +35,7 @@ class GetThief : Factory
 
 
 }
-class Person
+public class Person
 {
     public float _damage;
     private float _stamina;
@@ -124,6 +124,100 @@ class Person
     {
 
     }
+    public static void Battle(Person character)
+    {
+        int stam;
+        bool battle = false;
+        Random random = new Random();
+        int rnd = random.Next(0, 3);
+        Person[] enemys =
+        {
+            new Person(5, 50, 50, 100, 1, "орк"),
+            new Person(7, 60, 70, 100, 1, "троль"),
+            new Person(3, 30, 30, 100, 1, "слайм"),
+        };
+
+        Console.Write($"\nВаше здоровье: {character.Health}\nЗдоровье противника {enemys[rnd].Name}: {enemys[rnd].Health}\n");
+        ConsoleKeyInfo Selection;
+        character.ReturnSpell();
+
+        while (character.Health > 0 && enemys[rnd].Health > 0)
+        {
+            //Console.WriteLine($"\n\n{character.Armor}\n{character._SpellCounter}\n\n");
+            Console.WriteLine("На вас напали! Выпить зелье перед ходом(1) или перейти к ходу(2)?");
+            Selection = Console.ReadKey();
+            while (Selection.Key != ConsoleKey.D2)
+            { 
+                    if ((Selection.Key != ConsoleKey.D2)&&( character.Heal > 0))
+                    {
+                        character.DrinkHeal();
+                    }
+                //Console.WriteLine($"\n\n{character.Armor}\n{character._SpellCounter}\n\n");
+                Console.Write($"\nВаше здоровье: {character.Health}\nЗдоровье противника {enemys[rnd].Name}: {enemys[rnd].Health}\n");
+                Console.WriteLine("На вас напали! Выпить зелье перед ходом(1) или перейти к ходу(2)?");
+                Selection = Console.ReadKey();
+            }
+            if (Selection.Key == ConsoleKey.D2)
+            {
+                Console.WriteLine("Какое действие вы выберите?\nАтака - 1\nБлокировать атаку врага - 2\nСпециальная способность - 3");
+                Selection = Console.ReadKey();
+                while (battle == false)
+                {
+
+                    if (Selection.Key == ConsoleKey.D1)
+                    {
+                        if (character.Stamina > 19)
+                        {
+                            stam = -20;
+                            character.StaminaChanges(stam);
+                            enemys[rnd].TakeDamage(character.Damage);
+                        }
+                        else
+                        {
+                            Console.WriteLine("У вас недостаточно Выносливости! Вы пытаетесь попасть по врагу, но промахиваетесь! Враг вас атаковал!");
+                        }
+                    }
+                    if (Selection.Key == ConsoleKey.D2)
+                    {
+                        stam = 10;
+                        character.BlockDamage(enemys[rnd].Damage);
+                        character.StaminaChanges(stam);
+                    }
+                    if (Selection.Key == ConsoleKey.D3)
+                    {
+                        if (character.Stamina > 49)
+                        {
+                            stam = -50;
+                            character.StaminaChanges(stam);
+                            character.SpecialSkill();
+                        }
+                        else
+                        {
+                            Console.WriteLine("У вас недостаточно Выносливости! Вы пытаетесь скастовать способность, но у вас ничего не вышло! Враг вас атаковал!");
+                        }
+                    }
+                    Console.Write($"\nВаше здоровье: {character.Health}\nЗдоровье противника {enemys[rnd].Name}: {enemys[rnd].Health}\n");
+                    character.TakeDamage(enemys[rnd].Damage);
+                    if (character.Health <= 0)
+                    {
+                        Console.WriteLine("Вы умерли");
+                        battle = true;
+                        break;
+                    }
+                    if (enemys[rnd].Health <= 0)
+                    {
+                        character.ReturnSpell();
+                        Console.WriteLine("Вы победили");
+                        battle = true;
+                        break;
+                    }
+                    Console.WriteLine("Какое действие вы выберите?\nАтака - 1\nБлокировать атаку врага - 2\nСпециальная способность - 3");
+                    Selection = Console.ReadKey();
+                }
+            }
+            Console.WriteLine($"Ваше здоровье: {character.Health}\nВаша выносливость: {character.Stamina}\nЗдоровье противника {enemys[rnd].Name}: {enemys[rnd].Health}\n");
+        }
+    }
 }
 
 class Knight : Person
@@ -176,5 +270,8 @@ class Thief : Person
         _SpellCounter = 0;
     }
 }
+
+
+
 
 
